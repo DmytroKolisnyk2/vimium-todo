1064b307ccac637fa78c1ade54d848b38da2391c
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2024-07-22 22:07:34 +0300

    feat-(client): added parser tests

---
+++ client/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod console;
+pub mod task;
+pub mod auth;
+pub mod parser;
+pub mod menu;
---
+++ client/tests/parser_tests.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use vimium_todo_client::parser::{parse_error}; 
+
+
+    #[test]
+    fn test_parse_error_valid() {
+        let json_error = r#"{"error": {"message": "An error occurred"}}"#;
+        assert_eq!(parse_error(json_error), "An error occurred");
+    }
+
+    #[test]
+    fn test_parse_error_invalid() {
+        let json_error = "Not a JSON";
+        assert_eq!(parse_error(json_error), "Not a JSON");
+    }
+}
